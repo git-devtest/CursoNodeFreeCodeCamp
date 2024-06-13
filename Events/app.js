afc11f6490a69004f0667da14e817eb460109f1d
Jhon Harold Hincapie <jhonmaster+git@gmail.com>
2024-06-13 10:43:12 -0500

    Manejo de eventos en Node.js

--- Events/app.js
+++ Events/app.js
@@ -1,3 +1,25 @@
 const EventEmitter = require('events');
+//console.log('EventEmitter:', EventEmitter);
 
-console.log('EventEmitter:', EventEmitter);
+const emisorProductos = new EventEmitter();
+
+// Captura y ejecución del evento de compra
+emisorProductos.on('compra', () => {
+    console.log('Se ha realizado la compra de un producto');
+});
+
+// Captura y ejecución del evento de compra con valor
+emisorProductos.on('compraValor', (valor) => {
+    console.log(`Compra por valor de: $${valor}`);
+});
+
+// Captura y ejecución del evento de compra con valor y cantidad
+emisorProductos.on('compraValorCant', (valor, cantidad) => {
+    console.log(`Compra por valor de: $${valor}`);
+    console.log(`Número de productos: ${cantidad}`);
+});
+
+// Emisión del evento
+emisorProductos.emit('compra');
+emisorProductos.emit('compraValor', 25000);
+emisorProductos.emit('compraValorCant', 50000, 5);
