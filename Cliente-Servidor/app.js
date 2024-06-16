072a079f523ff347f51f1ea2187240bb9e3a4f28
Jhon Harold Hincapie <jhonmaster+git@gmail.com>
2024-06-16 15:30:08 -0500

    Ejercicios sobre Cliente-Servidor

--- Cliente-Servidor/app.js
+++ Cliente-Servidor/app.js
@@ -1,15 +1,19 @@
 const http = require('http');
 
 const servidor = http.createServer((req, res) => {
-    console.log('===> Response entregado');
-    /* console.log(req); // Imprime el objeto request
+    console.log('===> req (Request => Solicitud');
+    console.log(req); // Imprime el objeto request
     console.log(req.url); // Imprime la URL del request
     console.log(req.method); // Imprime el método del request
-    console.log(req.headers); // Imprime los headers del request   */ 
-    // console.log(res); // Imprime el objeto response
+    console.log(req.headers); // Imprime los headers del request
+    
+    console.log('===> res (Response => Respuesta');
+    console.log(res); // Imprime el objeto response
     console.log(res.statusCode); // Imprime el status code de la respuesta
-    res.setHeader('Content-Type', 'apllication/json');
+    res.setHeader('Content-Type', 'application/json');
     console.log(res.getHeaders()); // Imprime el header Content-Type
+    res.statusCode = 305; // Cambia el status code de la respuesta
+    console.log(res.statusCode); // Imprime el nuevo status code
 });
 
 const _puerto = 3000;
