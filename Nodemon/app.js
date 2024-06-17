c760a2bf81c9630d7c031bcbd8cffbcc5b4605de
Jhon Harold Hincapie <jhonmaster+git@gmail.com>
2024-06-17 15:29:09 -0500

    Ejercicio sobre instalación y uso de Nodemon

---
+++ Nodemon/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const _puerto = 3000;
+
+const server = http.createServer((req, res) => {
+   res.end('Hola freeCodeCamp! Bienvenidos a mi primer servidor en Node.js');
+});
+
+server.listen(_puerto, () => {
+	console.log(`Server running at http://localhost:${_puerto}/`);
+});
+
+// nodemos <nombre_archivo.js> para ejecutar el script automáticamente
